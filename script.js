dce6fe6447c951316d92741577ff2471747a1896
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-04-28 18:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 let k;
 function palindrome(string){
- const len = string.length;
+ const len = string.length();
     for (let i = 0; i < len / 2; i++) {
         if (string[i] !== string[len - 1 - i]) {
             return false;
